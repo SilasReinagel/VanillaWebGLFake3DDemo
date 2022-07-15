3f4e6f2d49fa1b59be4723e2ea0877d349945828
Silas Reinagel <silas.reinagel@carvana.com>
2022-07-15 10:41:52 -0500

    Initial Demo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vanilla JS - Fake 3D Image Effect with WebGL
+
+An interactive "fake" 3D effect for images with plain WebGL.
+
+[Original Source](https://github.com/akella/fake3d)
---
+++ js/buttons.js
@@ -0,0 +1,17 @@
+function appendHtml(el, str) {
+  var div = document.createElement('div');
+  div.innerHTML = str;
+  while (div.children.length > 0) {
+    el.appendChild(div.children[0]);
+  }
+}
+const html = `
+<div class="buttons">
+  <div class="button-29" role="button" onclick="location.href='./index.html';">Demo 1</div>
+  <div class="button-29" role="button" onclick="location.href='./index3.html';">Demo 2</div>
+  <div class="button-29" role="button" onclick="location.href='./index5.html';">Demo 3</div>
+  <div class="button-29" role="button" onclick="location.href='./index2.html';">Demo 4</div>
+</div>
+`;
+
+appendHtml(document.body, html);
